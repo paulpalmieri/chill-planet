c12df280d633446d3295f93745c68ba61bb3c470
Paul <paulpalmieri@outlook.fr>
2020-05-28 22:13:21 -0400

    Updates

---
+++ precache-manifest.5aa4f241c28198d053a3e7627ea01db7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4f0e443d87202c4f6287a55bcf0440",
+    "url": "/chill-planet/index.html"
+  },
+  {
+    "revision": "e2107fe99b8f44583285",
+    "url": "/chill-planet/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e97a8f209414aed9863d",
+    "url": "/chill-planet/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e2107fe99b8f44583285",
+    "url": "/chill-planet/static/js/2.7759fb55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chill-planet/static/js/2.7759fb55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e97a8f209414aed9863d",
+    "url": "/chill-planet/static/js/main.602e4f71.chunk.js"
+  },
+  {
+    "revision": "bb97c591c14c09dc31ce",
+    "url": "/chill-planet/static/js/runtime-main.8b78f17b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chill-planet/static/media/logo.5d5d9eef.svg"
+  }
+]);
